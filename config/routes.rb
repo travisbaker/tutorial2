34495f5b932e6d22b17368c8fc7b82e84d23e87d
Travis Baker <travis.baker@tribewire.com>
2010-11-03 10:00:48 +1100

    removed default rails page

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Tutorial2::Application.routes.draw do
+  match '/contact', :to => 'pages#contact'
+  match '/about',   :to => 'pages#about'
+  match '/help',    :to => 'pages#help'
+  
   get "pages/home"
 
   get "pages/contact"
@@ -6,6 +10,8 @@ Tutorial2::Application.routes.draw do
   get "pages/about"
 
   get "pages/help"
+  
+  
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
