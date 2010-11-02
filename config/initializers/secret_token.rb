655848b775197bafd924b6e3e5ccecb8a43ff1c0
Travis Baker <travis.baker@tribewire.com>
2010-11-02 12:16:15 +1100

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tutorial2::Application.config.secret_token = '961cd718d1b044dec2717e2ff40d06571c89fd29a9d9d728e99110dd9a9bf695e056e8496a0c9649d64fac5405f369fe17f308755dbceb081b603da230e27674'
