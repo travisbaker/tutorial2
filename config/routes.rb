cbcb6b1b149109ff0f1420cf8ccf619551848e73
Travis Baker <travis.baker@tribewire.com>
2010-11-03 11:11:46 +1100

    Chapter5 done

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,14 @@
 Tutorial2::Application.routes.draw do
+  get "users/new"
+
   match '/contact', :to => 'pages#contact'
   match '/about',   :to => 'pages#about'
   match '/help',    :to => 'pages#help'
+  match '/signup',  :to => 'users#new'
   
-  get "pages/home"
-
-  get "pages/contact"
-  
-  get "pages/about"
-
-  get "pages/help"
-  
+  # You can have the root of your site routed with "root"
+  # just remember to delete public/index.html.
+  root :to => "pages#home"
   
   # The priority is based upon order of creation:
   # first created -> highest priority.
@@ -59,9 +57,7 @@ Tutorial2::Application.routes.draw do
   #     resources :products
   #   end
 
-  # You can have the root of your site routed with "root"
-  # just remember to delete public/index.html.
-  # root :to => "welcome#index"
+  
 
   # See how all your routes lay out with "rake routes"
 
